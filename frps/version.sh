f5d3c0a42b3c1eb74b2294e0cdd5c910bb12780d
jk0106 <lgw940106@gmail.com>
2018-06-23 17:43:54 +0800

    Update version.sh

--- frps/version.sh
+++ frps/version.sh
@@ -2,4 +2,4 @@
 export FRPS_VER=0.16.1
 export FRPS_INIT="https://raw.githubusercontent.com/jk0106/frp-one/master/frps/frps.init"
 export aliyun_download_url="https://code.aliyun.com/clangcn/frp/raw/master"
-export github_download_url="https://github.com/fatedier/frp/releases/download"
+export github_download_url="https://github.com/fatedier/frp/releases/download/v0.16.1/frp_0.16.1_linux_amd64.tar.gz"
